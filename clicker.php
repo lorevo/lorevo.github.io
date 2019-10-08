4df134d31c8df4db00496dee874808226d89755a
lorevo <50034487+lorevo@users.noreply.github.com>
2019-10-08 15:49:15 +0200

    Update and rename index.php to clicker.php

---
+++ clicker.php
@@ -0,0 +1,13 @@
+<?php
+include 'db.php';
+$pseudo=$_GET['pseudo'];
+$sql = $conn->prepare("INSERT INTO emp (pseudo) VALUES (?, ?)");
+$sql->bind_param("is", $pseudo);
+$result = $sql->execute();
+if (!$result) {
+	echo "Error inserting data";
+	$sql->close();
+} else {
+	 echo "Data inserted successfully";
+	 $sql->close();
+ }
